f2ecd91d809718765c627791f8c2e87097c1b6df
Francesco De Luca <francy.delc01@gmail.com>
2025-07-18 16:36:56 +0200

    Modifica alle stampe per uniformità col refactoring

--- pages/Compare Fastest Laps.py
+++ pages/Compare Fastest Laps.py
@@ -71,7 +71,7 @@ def plot_driver(ax, l):
 
     def extract_text_from_speed_delta(speed_delta_tuple):
         # Estrae il testo dalla tupla di velocità
-        return f"{speed_delta_tuple[0]} Km/h - {speed_delta_tuple[1]} Km/h (\u0394 = -{speed_delta_tuple[2]} km/h)"
+        return f"{speed_delta_tuple[0]} Km/h→{speed_delta_tuple[1]} Km/h (\u0394={speed_delta_tuple[2]} km/h)"
 
     x, y = tel.loc[mask,'X'], tel.loc[mask,'Y'] # x e y contengono le coordinate X e Y della posizione nel settore specificato del tracciato ed ottenuto dalla maschera
     thr = tel.loc[mask,'Throttle'] # thr contiene i valori dell'acceleratore nel settore specificato
@@ -183,7 +183,7 @@ def plot_driver(ax, l):
     if match:
         lap_time = match.group(2)
     # Descrizione di ogni grafico
-    ax.set_title(f"{l['Driver']} - {weekend.EventName} - {session.event.year} - {ses} - Turn {sector.loc[0,'Number']}\n Compound: {l['Compound']} - LAP: {l['LapNumber'].astype(int)}\n LAP Time: {lap_time}",
+    ax.set_title(f"{l['Driver']} - {weekend.EventName} - {session.event.year} - {ses} - Sector {sector.loc[0,'Number']}\n Compound: {l['Compound']} - LAP: {l['LapNumber'].astype(int)}\n LAP Time: {lap_time}",
                  fontsize=8, fontweight='bold')
 
 # --- Sidebar per controlli ---
